cd083c53405584a1a0b04ec2f4b8f785aa99ad86
Shivang Mishra <shivang978@gmail.com>
2024-03-31 02:26:29 +0530

    improved navigation in explorer

--- src/ui/pages/LsStorage.tsx
+++ src/ui/pages/LsStorage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { DirectoryMetadata, ResourceMetadata } from "src/grpc/mftAgentStubs";
 import { lsStorage } from "src/grpc/storage";
 import FileExplorer from "../components/FileExplorer";
@@ -8,7 +8,7 @@ import { ArrowBack, ArrowForward } from "@mui/icons-material";
 
 export default function LsStorage() {
   const location = useLocation();
-
+  const navigate = useNavigate();
   const [currentIdx, setCurrentIdx] = React.useState<number>(0);
   const [resourceMetadataList, setResourceMetadataList] = React.useState<
     ResourceMetadata[]
@@ -19,29 +19,21 @@ export default function LsStorage() {
 
   const loadStorage = async (storage: StorageEntry) => {
     const lsResponse = await window.api.lsStorage(storage.storageId);
-    console.log("lsResponse", JSON.stringify(lsResponse));
     const files = lsResponse.directory.files;
     setStorage(storage);
     setResourceMetadataList([lsResponse]);
   };
 
   const selectDirectory = async (directory: DirectoryMetadata) => {
-    console.log("selectDirectory", JSON.stringify(directory.resourcePath));
-    console.log(
-      "all directories",
-      JSON.stringify(resourceMetadataList.map((x) => x.directory.resourcePath))
-    );
     if (
       resourceMetadataList.filter(
         (resourceMetadata) =>
           resourceMetadata.directory.resourcePath === directory.resourcePath
       ).length > 0
     ) {
-      console.log("Already loaded", JSON.stringify(directory));
       setCurrentIdx(currentIdx + 1);
       return;
     }
-    console.log("selectDirectory", JSON.stringify(directory));
     const lsResponse = await window.api.lsStorage(
       storage.storageId,
       directory.resourcePath
@@ -61,20 +53,27 @@ export default function LsStorage() {
     <Paper>
       <Box>
         <IconButton
-          disabled={currentIdx === 0}
-          onClick={() => setCurrentIdx(currentIdx - 1)}
+          onClick={() => {
+            if (currentIdx === 0) {
+              navigate("/");
+              return;
+            }
+            setCurrentIdx(currentIdx - 1);
+          }}
         >
           <ArrowBack />
         </IconButton>
         <IconButton
-          disabled={currentIdx === resourceMetadataList.length - 1}
+          disabled={
+            resourceMetadataList.length === 0 ||
+            currentIdx === resourceMetadataList.length - 1
+          }
           onClick={() => setCurrentIdx(currentIdx + 1)}
         >
           <ArrowForward />
         </IconButton>
         <Typography variant="h4" component="h4" marginBottom="0.5rem">
-        {storage?.storageName ||
-            location.state.storage.storageName}/
+          {storage?.storageName || location.state.storage.storageName}/
           {resourceMetadataList[currentIdx]?.directory.resourcePath}
         </Typography>
       </Box>
