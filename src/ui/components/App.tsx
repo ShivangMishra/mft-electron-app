ad50ab593ae622e37083348ca4c72739c42788e2
Shivang Mishra <shivang978@gmail.com>
2024-03-30 12:51:03 +0530

    added React and MUI

---
+++ src/ui/components/App.tsx
@@ -0,0 +1,7 @@
+import { Title } from "@mui/icons-material";
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function App() {
+  return <Button variant="contained">Hello world</Button>;
+}
