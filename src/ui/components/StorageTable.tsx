1b8fc0c81600759c31da01d7e05afa7f6cff9b7b
Shivang Mishra <shivang978@gmail.com>
2024-03-31 12:45:24 +0530

    replaced table with DataGrid in StorageTable

--- src/ui/components/StorageTable.tsx
+++ src/ui/components/StorageTable.tsx
@@ -6,7 +6,13 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridValidRowModel,
+} from "@mui/x-data-grid";
 
 enum StorageType {
   S3 = 0,
@@ -24,47 +30,55 @@ enum StorageType {
 
 export default function StorageTable(props: {
   storageList: StorageEntry[];
-  selectStorage: (storage: StorageEntry) => void;
+  selectStorages: (storages: StorageEntry[]) => void;
+  openStorage: (storage: StorageEntry) => void;
 }) {
-  const { storageList, selectStorage } = props;
+  const { storageList, selectStorages, openStorage } = props;
+  const columns: GridColDef[] = [
+    {
+      field: "storageName",
+      headerName: "Storage Name",
+      width: 150,
+      renderCell: (params: GridRenderCellParams<any, string>) => (
+        <Button
+          sx={{ marginTop: 0, marginBottom: 0, textTransform: "none" }}
+          onClick={() => {
+            console.log("params", params);
+            console.log(params.value);
+            openStorage(params.row as StorageEntry);
+          }}
+        >
+          {params.value}
+        </Button>
+      ),
+    },
+    { field: "storageType", headerName: "Storage Type", width: 130,
+    valueGetter: (value) => {
+        return StorageType[value as StorageType];
+      }
+  },
+    { field: "storageId", headerName: "Storage ID", width: 300 },
+  ];
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Storage Name</TableCell>
-            <TableCell align="left">Storage Type</TableCell>
-            <TableCell align="left">Storage ID</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {storageList.map((storage) => (
-            <TableRow
-              key={storage.storageName}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell
-                component="th"
-                scope="row"
-                sx={{ paddingTop: 0, paddingBottom: 0 }}
-              >
-                <Button
-                  sx={{ marginTop: 0, marginBottom: 0, textTransform: "none"}}
-                  onClick={() => {
-                    selectStorage(storage);
-                  }}
-                >
-                  {storage.storageName}
-                </Button>
-              </TableCell>
-              <TableCell align="left">
-                {StorageType[storage.storageType]}
-              </TableCell>
-              <TableCell align="left">{storage.storageId}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box sx={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={storageList}
+        columns={columns}
+        getRowId={(row) => row.storageId}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        onRowSelectionModelChange={(selection: GridValidRowModel) => {
+            const selectedStorages = storageList.filter((storage) =>
+              selection.includes(storage.storageId)
+            )
+            selectStorages(selectedStorages);
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </Box>
   );
 }
